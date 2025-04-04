c93bd1cc6d2be87260b0f56f1c9df61bf524c2e9
Igor CHIGRIN <broman6700@gmail.com>
2025-04-04 21:26:27 +1000

    "admin possibilities expanded"

--- coursenator70000/quizzes/admin.py
+++ coursenator70000/quizzes/admin.py
@@ -15,28 +15,29 @@ class QuizQuestionInline(admin.TabularInline):
 
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ["title", "number", "total_quiz_questions", "display_course", "course_order", "first_quiz", "pass_score", "is_complete_required"]
-    list_editable = ["number", "course_order", "pass_score", "is_complete_required"]
-    search_fields = ["title"]
-    inlines = [QuizQuestionInline]
-    actions = [deactivate_complete_req_field]
-
-    def first_quiz(self, obj):
-        return obj.course_order == 2
-
-    first_quiz.short_description = "Первяк"
-    first_quiz.boolean = True
-
-    def total_quiz_questions(self, obj):
-        return QuizQuestion.objects.filter(quiz=obj).count()
-
-    total_quiz_questions.short_description = "Кол-во вопросов"
-
-    def display_course(self, obj):
-        link = reverse("admin:courses_course_change", args=[obj.course.id])
-        return format_html('<a href="{}">{}</a>', link, obj.course)
-
-    display_course.short_description = "Курс!"
+    pass
+    # list_display = ["title", "number", "total_quiz_questions", "display_module", "course_order", "first_quiz", "pass_score", "is_complete_required"]
+    # list_editable = ["number", "course_order", "pass_score", "is_complete_required"]
+    # search_fields = ["title"]
+    # inlines = [QuizQuestionInline]
+    # actions = [deactivate_complete_req_field]
+    #
+    # def first_quiz(self, obj):
+    #     return obj.course_order == 2
+    #
+    # first_quiz.short_description = "Первяк"
+    # first_quiz.boolean = True
+    #
+    # def total_quiz_questions(self, obj):
+    #     return QuizQuestion.objects.filter(quiz=obj).count()
+    #
+    # total_quiz_questions.short_description = "Кол-во вопросов"
+    #
+    # def display_course(self, obj):
+    #     link = reverse("admin:courses_course_change", args=[obj.course.id])
+    #     return format_html('<a href="{}">{}</a>', link, obj.course)
+    #
+    # display_course.short_description = "Курс!"
 
 
 @admin.register(QuizQuestion)
--- coursenator70000/users/models.py
+++ coursenator70000/users/models.py
@@ -17,27 +17,3 @@ class User(AbstractUser):
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
-class UserLessonCompleted(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
-    lesson = models.ForeignKey('courses.Lesson', on_delete=models.CASCADE)
-    completed_datetime = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.student, self.lesson, self.completed_datetime
-
-    class Meta:
-        verbose_name = "Завершенный урок"
-        verbose_name_plural = "Завершенные уроки"
-
-class UserQuizAttempt(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
-    quiz = models.ForeignKey('quizzes.Quiz', on_delete=models.CASCADE)
-    attempt_datetime = models.DateTimeField()
-    score = models.IntegerField()
-
-    def __str__(self):
-        return f"Student: {self.student}, Quiz: {self.quiz}, Date: {self.attempt_datetime}, Score: {self.score}"
-
-    class Meta:
-        verbose_name = "Попытка выполнить квиз"
-        verbose_name_plural = "Попытки выполнить квизы"
